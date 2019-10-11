f042e210551f91578b438c8db8cf9cd79daafebc
Keifer Hofacker <crazyquesadilla@gmail.com>
2019-10-11 11:56:10 -0700

    added some error handling and parameters

--- Windows/Powershell/PSDirStat.ps1
+++ Windows/Powershell/PSDirStat.ps1
@@ -1,8 +1,20 @@
+param ($driveletter = "C")
+
 function Get-FolderSize {
     param ($folder)
     [math]::round(((robocopy $folder C:\temp\ /L /XJ /R:0 /NP /E /BYTES /NFL /NDL /NJH /MT:64)[-4] -replace "\D+(\d+).*",'$1')/1GB,3)
+    # most importantly, /L means that this only logs
 }
     
- 
-$children = get-childitem C:\ -Depth 1 -ErrorAction SilentlyContinue 
-$children | Select-Object -property fullname,@{name="foldersize";expr={Get-FolderSize $_.fullname}} | Sort-Object foldersize -descending | Select-Object -first 10
+
+try{
+    $path = $("$driveletter`:\")
+    if(!(Test-Path $path)){
+        Write-Error "$path does not exist"; exit 1001
+    }
+    $children = get-childitem $("$driveletter`:\") -Depth 1 -ErrorAction Continue
+
+    $children | Select-Object -property fullname,@{name="foldersize";expr={Get-FolderSize $_.fullname}} | Sort-Object foldersize -descending | Select-Object -first 10
+    exit 0
+}
+catch {Write-Output "Script failed:`n",$_.Exception.Message; exit 1001}
